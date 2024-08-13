0840f41077e8f22b508c8da11458a355c1865417
BlackPri0r <160561469+BlackPri0r@users.noreply.github.com>
2024-08-13 13:38:39 +1200

    initial commit

---
+++ src/Card.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Card(){
+    return(
+        <>
+        <div className="w-[252px] h-[344px] bg-[#1a1a1a] justify-center flex rounded-lg flex-col">
+            <div className="text-3xl text-center">KYS</div>
+        </div>
+        </>
+    )
+}
+
+export default Card
