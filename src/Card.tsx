c0f35ecc43cfee81d22429fe5fe088fb8365ff87
BlackPri0r <160561469+BlackPri0r@users.noreply.github.com>
2024-08-14 12:42:49 +1200

    made cardMaker function

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,24 +1,22 @@
 import React from "react";
 
-function Card(){
-    return(
-        <>
-        <div className="w-[252px] h-[344px] bg-[#1a1a1a] flex rounded-lg flex-col">
-            {/* {image and button} */}
-            <div className="relative w-[252px] h-[242px] rounded-lg flex flex-col items-center">
-                <img className="rounded-md" alt="image" src="src/assets/images/image-baklava-desktop.jpg"/>
-                <button className="absolute bottom-[-20px] left-1/2 transform -translate-x-1/2 h-11 w-40 rounded-full border border-[#8a8a8a]">
-                    <p>Add to Cart</p>
-                </button>
-            </div>
+function Card({ imageSrc, name, category, price }) {
+  return (
+    <div className="w-[252px] h-[344px] bg-[#1a1a1a] flex rounded-lg flex-col">
+      {/* Image and button */}
+      <div className="relative w-[252px] h-[242px] rounded-lg flex flex-col items-center">
+        <img className="rounded-md" alt={name} src={imageSrc} />
+        <button className="absolute bottom-[-20px] left-1/2 transform -translate-x-1/2 h-11 w-40 rounded-full border border-[#8a8a8a]">
+          <p>Add to Cart</p>
+        </button>
+      </div>
 
-            {/* {bottom text} */}
-            <div className="text-[14px] mt-5">Item</div>
-            <p className="text-[15px]">Product</p>
-            <p className="text-[15px]">$Cost</p>
-        </div>
-        </>
-    )
+      {/* Bottom text */}
+      <div className="text-[14px] mt-5">{name}</div>
+      <p className="text-[15px]">{category}</p>
+      <p className="text-[15px]">${price.toFixed(2)}</p>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
