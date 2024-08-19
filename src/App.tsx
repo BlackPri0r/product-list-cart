fc39d852f593082cf8fdc2dcb38a351e909384f0
BlackPri0r <160561469+BlackPri0r@users.noreply.github.com>
2024-08-20 11:44:03 +1200

    working unoptimized cart

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,9 @@ function App() {
                 <div>{item.name}</div>
                 <div>Qty: {item.quantity}</div>
                 <div>${(item.price * item.quantity).toFixed(2)}</div>
-                <button onClick={() => removeFromCart(item.name)}>Remove</button>
+                <button className="flex items-center justify-center" onClick={() => removeFromCart(item.name)}>
+                  <svg className="h-4, w-4" xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="none" viewBox="0 0 10 10"><path fill="#CAAFA7" d="M8.375 9.375 5 6 1.625 9.375l-1-1L4 5 .625 1.625l1-1L5 4 8.375.625l1 1L6 5l3.375 3.375-1 1Z"/></svg>
+                </button>
               </div>
             ))}
             <div className="mt-4">Total: ${getTotalPrice().toFixed(2)}</div>
